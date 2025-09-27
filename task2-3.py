21606c3c2eacf9b9286f1fdde880a3f11aff88b5
Vitalina <91380052+VitalinaAlex@users.noreply.github.com>
2025-09-27 11:19:48 +0300

    Add files via upload

---
+++ task2-3.py
@@ -0,0 +1,24 @@
+# hello world
+print("hello world")
+# hello*hello*hello
+print("hello","hello","hello",sep='*')
+#hello hello hello-Hello, Class
+print("hello","hello","hello",end='-')
+print("Hello", end=", ")
+print("Class")
+#Hello
+#Class
+print("Hello", end="\n")
+print("Class")
+#O
+print("#" * 9)
+for i in range(5):
+    print("#\t#")
+print("#" * 9)
+print()
+#H
+for i in range(3):
+    print("#\t#")
+print("#" * 9)
+for i in range(3):
+    print("#\t#")

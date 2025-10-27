0f042962386523cfa6082af78a68e89ea7506ef9
Vitalina <91380052+VitalinaAlex@users.noreply.github.com>
2025-10-27 20:26:54 +0200

    Update 14_23_10_decorators.py

--- 14_23_10_decorators.py
+++ 14_23_10_decorators.py
@@ -54,21 +54,29 @@ create_slogan("Steve")
 """Task 3
 Write a decorator 'arg_rules' that validates arguments passed to the function.
 A decorator should take 3 arguments:
-
 max_length: 15
 type_: str
-contains: []  - list of symbols that an argument should contain
-If some of the rules' checks returns False, the function should return False and print the reason it failed; otherwise, return the result."""
+contains: []  - list of symbols that an argument should contain"""
+#If some of the rules' checks returns False, the function should return False and print the reason it failed; otherwise, return the result.
 
-"""
 def arg_rules(type_: type, max_length: int, contains: list):
-
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            for symbol in contains:
+                if symbol not in text:
+                    print(f"Missing symbol: {symbol}")
+            if not isinstance(arg, type_):
+                print("Type check failed")
+                return False
+            if len(some_string) > max_length or len(some_string)==0:
+                print("Too long or == 0!")
+            return wrapper
+        return decorator
     pass
 
 @arg_rules(type_=str, max_length=15, contains=['05', '@'])
 def create_slogan(name: str) -> str:
-
     return f"{name} drinks pepsi in his brand new BMW!"
 
-assert create_slogan('johndoe05@gmail.com') is False
-assert create_slogan('S@SH05') == 'S@SH05 drinks pepsi in his brand new BMW!'"""
+assert create_slogan('johndoe05@gmail.com') is False, "False"
+assert create_slogan('S@SH05') == 'S@SH05 drinks pepsi in his brand new BMW!', "True"

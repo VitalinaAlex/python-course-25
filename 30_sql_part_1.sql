41f8801568820f59ceb894cf7c664dbd80f001ed
Vitalina <91380052+VitalinaAlex@users.noreply.github.com>
2025-11-22 13:47:19 +0200

    zdrhd
    Added multiple select queries to retrieve employee information, including names, unique department IDs, and salary calculations.

--- 30_sql_part_1.sql
+++ 30_sql_part_1.sql
@@ -27,3 +27,34 @@ DELETE FROM customers
 WHERE name = 'Linka';
 
 SELECT * FROM customers;
+
+/*Task 2
+Select queries
+Use the sample SQLite database hr.db
+As a solution to HW, create a file named: task2.sql with all SQL queries:*/
+--write a query to display the names (first_name, last_name) using alias name "First Name", "Last Name" from the table of employees;
+SELECT 
+    first_name AS "First Name",
+    last_name AS "Last Name"
+FROM employees;
+--write a query to get the unique department ID from the employee table
+SELECT DISTINCT department_id as dep_id
+FROM employees
+order by department_id;
+--write a query to get all employee details from the employee table ordered by first name, descending
+SELECT *
+FROM employees
+order by first_name desc;
+--write a query to get the names (first_name, last_name), salary, PF of all the employees (PF is calculated as 12% of salary)
+SELECT first_name, last_name, salary, salary*0.12 as PF
+FROM employees
+order by employee_id;
+--write a query to get the maximum and minimum salary from the employees table
+SELECT MAX(salary) as MAX_SALARY, MIN(salary) as MIN_SALARY
+FROM employees;
+--write a query to get a monthly salary (round 2 decimal places) of each and every employee
+SELECT 
+    first_name,
+    last_name,
+    ROUND(salary / 12.0, 2) AS "Monthly Salary"
+FROM employees;

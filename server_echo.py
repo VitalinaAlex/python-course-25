980bc0adb6e46f80a00ef3dc4b291cffab11e509
Vitalina Alex <vitalinaaleksieienko@gmail.com>
2025-12-16 19:35:10 +0200

    echo2

--- server_echo.py
+++ server_echo.py
@@ -12,7 +12,7 @@ def handle_client(conn, addr):
     print(f"Disconnected: {addr}")
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind("127.0.0.1", 65432)
+server.bind(("127.0.0.1", 65432))
 server.listen()
 
 print("Echo server is running...")

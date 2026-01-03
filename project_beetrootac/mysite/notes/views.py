ad287360c1e8369ab72543b3794fd11cc72b2b5c
macbook <_vitalina@ukr.net>
2026-01-03 23:53:44 +0200

    notes

---
+++ project_beetrootac/mysite/notes/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import Note
+
+def index(request):
+    notes = Note.objects.all()
+    return render(request, 'notes/index.html', {'notes': notes})
+
+
+# Create your views here.

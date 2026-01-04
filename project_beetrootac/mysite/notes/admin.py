7dad07010a0669515eb1245dcefaa147a850364f
macbook <_vitalina@ukr.net>
2026-01-04 13:16:58 +0200

    upd hw

--- project_beetrootac/mysite/notes/admin.py
+++ project_beetrootac/mysite/notes/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from .models import Note, Category
 
-admin.site.register(Note)
-admin.site.register(Category)
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'reminder')
+    list_filter = ('category', 'reminder')
+    search_fields = ('title',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    search_fields = ('title',)
 
 # Register your models here.
--- project_beetrootac/mysite/polls/admin.py
+++ project_beetrootac/mysite/polls/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date')
+    list_filter = ('pub_date',)
+    search_fields = ('question_text',)
+
+@admin.register(Choice)
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ('choice_text', 'question', 'votes')
+    list_filter = ('question',)
+    search_fields = ('choice_text',)
+
 # Register your models here.
